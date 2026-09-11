a98ef7877c1f5c012f2e22ee1c08c394d58036db
shyakabriella <lindabriella@gmail.com>
2026-09-11 08:21:59 +0200

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,16 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-        ]);
-    }
-        $this->call([
             RwandaLocationsSeeder::class,
         ]);
+    }
 }
